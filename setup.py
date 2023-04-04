c22dccbde2c598bd167aff30b4dc1b98ae97877d
Kirill <kasurkov2797@gmail.com>
2023-04-04 18:12:53 +0400

    Добавил стантдартную схему проекта из cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Goal of the project is to make a model capable of predicting results of professional footbal plays',
+    author='Kirill_Surkov',
+    license='',
+)
