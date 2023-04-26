105f451052e47229ca227848608ec054173ea69a
Kirill <kasurkov2797@gmail.com>
2023-04-26 21:36:43 +0400

    Default cookiecutter structure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='https://ods.ai/tracks/ml-in-production-spring-23',
+    author='K.A. Surkov',
+    license='MIT',
+)
